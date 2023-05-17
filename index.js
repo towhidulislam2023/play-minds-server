3edce2d45970398a05397d03b7866e3b44e19386
TOWHIDul Islam <towhidulislam2.bd@gmail.com>
2023-05-18 03:33:49 +0600

    and done shop by catagory

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const cors = require('cors');
 require('dotenv').config()
 app.use(cors())
 app.use(express.json())
+const imageData=require("./fakedata/gimage.json")
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+app.get('/gallery', (req, res) => {
+    res.send(imageData)
 })
 
 // console.log(process.env.DB_ACCESS_USER_ID);
@@ -29,9 +33,27 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
-        
+        const playMindsToysCollection = client.db("PlaymindsHub").collection("playmindsToysCollection")
+
+        app.get("/toys", async (req,res)=>{
+            const reqinfo=req.query
+            console.log(reqinfo.subcategory);
+            const query = { subcategory: reqinfo.subcategory }
+
+            const options = {
+                // sort returned documents in ascending order by title (A->Z)
+                // sort: { title: 1 },
+                // Include only the `title` and `imdb` fields in each returned document
+                projection: { _id: 1, name: 1, pictureURL: 1, price: 1, rating:1 },
+            };
+
+            const cursor = playMindsToysCollection.find(query, options)
+            const result=await cursor.toArray()
+            res.send(result)
+        })
+
 
 
 
