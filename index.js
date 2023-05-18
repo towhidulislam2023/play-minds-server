6af2e45664100fe6df203fb7d3b5bc24d029a4d7
TOWHIDul Islam <towhidulislam2.bd@gmail.com>
2023-05-18 16:52:14 +0600

    new routes saved for all toys page

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT||5000
+const port = process.env.PORT || 5000
 const cors = require('cors');
 require('dotenv').config()
 app.use(cors())
 app.use(express.json())
-const imageData=require("./fakedata/gimage.json")
+const imageData = require("./fakedata/gimage.json")
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -37,8 +37,8 @@ async function run() {
 
         const playMindsToysCollection = client.db("PlaymindsHub").collection("playmindsToysCollection")
 
-        app.get("/toys", async (req,res)=>{
-            const reqinfo=req.query
+        app.get("/toys", async (req, res) => {
+            const reqinfo = req.query
             console.log(reqinfo.subcategory);
             const query = { subcategory: reqinfo.subcategory }
 
@@ -46,18 +46,52 @@ async function run() {
                 // sort returned documents in ascending order by title (A->Z)
                 // sort: { title: 1 },
                 // Include only the `title` and `imdb` fields in each returned document
-                projection: { _id: 1, name: 1, pictureURL: 1, price: 1, rating:1 },
+                projection: { _id: 1, name: 1, pictureURL: 1, price: 1, rating: 1 },
             };
 
             const cursor = playMindsToysCollection.find(query, options)
-            const result=await cursor.toArray()
+            const result = await cursor.toArray()
             res.send(result)
         })
+        // app.get("/alltoys", async (req, res) => {
+        //     const limit = req.query.limit || 2;
 
-        app.get("/viewdetails/:id", async (req,res)=>{
-            const id=req.params.id
-            const query={_id: new ObjectId(id)}
-            const result =await playMindsToysCollection.findOne(query)
+        //     const projection = { _id: 1, pictureURL: 1, name: 1, price: 1, sellerEmail: 1, sellerName: 1, subcategory: 1, availableQuantity: 1 };
+
+        //     const result = await playMindsToysCollection.find().limit(parseInt(limit)).project(projection).toArray();
+
+        //     res.send(result);
+        // });
+
+        app.get("/alltoys", async (req, res) => {
+            const limit = req.query.limit || 2;
+            const sortType = req.query.sort || ""
+            const searchTerm = req.query.title || "";
+            let sortOrder;
+            if (sortType === "ascending") {
+                sortOrder = 1;
+            } else if (sortType === "descending") {
+                sortOrder = -1;
+            }
+            const searchFilter = searchTerm.trim() !== "" ? { name: { $regex: searchTerm, $options: "i" } } : {};
+            const options = {
+                sort: { price: sortOrder },
+                projection: { _id: 1, pictureURL: 1, name: 1, price: 1, sellerEmail: 1, sellerName: 1, subcategory: 1, availableQuantity: 1 }
+            };
+
+            const result = await playMindsToysCollection.find(searchFilter, options).limit(parseInt(limit)).toArray();
+            res.send(result);
+        });
+
+
+
+
+
+
+        app.get("/viewdetails/:id", async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await playMindsToysCollection.findOne(query)
             res.send(result)
         })
 
